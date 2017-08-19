49f7421e1dfba48d2a6b6c0f6d9e7cae10953364
juliodevelops <juliodevelops@gmail.com>
2017-08-19 20:31:22 +0000

    Index/show added

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,16 +1,24 @@
 class JobsController < ApplicationController
+    before_action :find_job, only: [:show, :edit, :update, :destroy]
     
     def index
-        @jobs = Job.all 
+        @jobs = Job.all.order("created_at DESC")
     end
     
     def show
     end
     
     def new
+        @job = Job.new
     end
     
     def create
+        @job = Job.new(jobs_params)
+        if @job.save
+            redirect_to @job 
+        else 
+            render "New"
+        end
     end
     
     def edit
@@ -22,8 +30,14 @@ class JobsController < ApplicationController
     def destroy
     end
     
+    private 
     
+    def jobs_params
+        params.require(:job).permit(:title, :description, :company, :url)
+    end
     
-    
+    def find_job
+        @job = Job.find(params[:id])
+    end
     
 end
